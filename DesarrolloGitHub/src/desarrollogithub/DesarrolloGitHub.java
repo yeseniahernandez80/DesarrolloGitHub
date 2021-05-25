ebc116cc320d8fc6dc15b36f672e099ba1026b99
yesenia hernandez <yesenia hernandez@LAPTOP-9D3HKF91>
2021-05-24 22:28:47 -0300

    Proyecto inicial con GitHub

---
+++ DesarrolloGitHub/src/desarrollogithub/DesarrolloGitHub.java
@@ -0,0 +1,16 @@
+
+package desarrollogithub;
+
+/**
+ *Nombre de la clase: DesarrolloGitHub
+ * Fecha: 24-05-2021
+ * CopyRigth: ITCA-FEPADE
+ * @author yesenia hernandez
+ */
+public class DesarrolloGitHub {
+
+    public static void main(String[] args) {
+        System.err.println("Inicializando proyecto con GitHub");
+    }
+    
+}
